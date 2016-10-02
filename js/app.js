a9c43db7fd8f10982ae2d245345791e0f06bc50f
pangj <jianxiong.pang@gmail.com>
2016-10-02 20:07:54 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+(function () {
+  'use strict';
 
+  /**
+   * This is the main module for our appliation
+   */
+  var appModule =  angular.module('app', ['ngComponentRouter', 'movies', 'homeComponent', 'about'])
+    .config(function($locationProvider) {
+	    $locationProvider.html5Mode(true);
+	  })
+	.value('$routerRootComponent', 'app')
+    .component('app', {
+	  templateUrl: '/js/components/appComponent.html',
+	  $routeConfig: [
+	    { path: '/home', component: 'homeComponent', name: 'Home', useAsDefault: true },
+	    { path: '/about',component: 'aboutComponent', name : 'About'},
+	    { path: '/movies/...', component: 'movies', name: 'Movies' },	    
+	    { path: '/**', redirectTo: ['Home'] }
+	  ]
+	})
+; 
+  
+
+  
+})();
